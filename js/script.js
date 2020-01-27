839150d6190b4f8e9b1c55c64423d44c91299354
Peter Androsov <piter2074@yandex.com>
2020-01-27 07:25:19 +0300

    Code reformat and optimization

--- js/script.js
+++ js/script.js
@@ -1,63 +1,58 @@
-window.addEventListener('DOMContentLoaded', function(){
-    // get the canvas DOM element
-    var canvas = document.getElementById('renderCanvas');
-    //canvas.height = 100;
-    //canvas.width = 100;
+window.addEventListener('DOMContentLoaded', function () {
+    let canvas = document.getElementById('renderCanvas');
+    let engine = new BABYLON.Engine(canvas, true);
 
-    // load the 3D engine
-    var engine = new BABYLON.Engine(canvas, true);
-
-    // createScene function that creates and return the scene
-    var createScene = function () {
-        var scene = new BABYLON.Scene(engine);
-
-        //Adding a light
-        var light = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 0, -10), scene);
-        light.intensity = 3;
-
-        var camera = new BABYLON.ArcRotateCamera("Camera", 2 * Math.PI / 2, 3 * Math.PI / 8, 50, BABYLON.Vector3.Zero(), scene);
+    let createScene = function () {
+        let scene = new BABYLON.Scene(engine);
+        let light = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 0, -10), scene);
+        let camera = new BABYLON.ArcRotateCamera("Camera", 2 * Math.PI / 2, 3 * Math.PI / 8, 50, BABYLON.Vector3.Zero(), scene);
+        let assetsManager = new BABYLON.AssetsManager(scene);
+        let meshTask = assetsManager.addMeshTask("earth", "", "./earth/", "scene_final.glb");
+        let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 0.5}, scene);
 
         camera.attachControl(canvas, true);
+        camera.useBouncingBehavior = true;
 
+        light.intensity = 3;
         camera.lowerRadiusLimit = 15;
         camera.upperRadiusLimit = 35;
+        sphere.material = new BABYLON.StandardMaterial("pin_mat", scene);
+        sphere.position = new BABYLON.Vector3(0, 10, 0);
 
-        camera.useBouncingBehavior = true;
-        var assetsManager = new BABYLON.AssetsManager(scene);
-        var meshTask = assetsManager.addMeshTask("skull task", "", "./earth/", "scene_final.glb");
-        //var box = BABYLON.Mesh.CreateBox("box", 5.0, scene);
-        meshTask.onSuccess = function (task) {
-            task.loadedMeshes[0].position = new BABYLON.Vector3.Zero();
-            //alert(task.loadedMeshes[0].position);
-        };
+        let selected = null;
+        scene.onPointerObservable.add(function (evt) {
+            if (selected) {
+                selected.material.diffuseColor = BABYLON.Color3.Gray();
+                selected = null;
+            }
+            if (evt.pickInfo.hit && evt.pickInfo.pickedMesh && evt.event.button === 0) {
+                selected = evt.pickInfo.pickedMesh;
+                evt.pickInfo.pickedMesh.material.diffuseColor = BABYLON.Color3.Red();
+            }
+        }, BABYLON.PointerEventTypes.POINTERUP);
 
+        meshTask.onSuccess = (task) => task.loadedMeshes[0].position = new BABYLON.Vector3.Zero();
 
-        // Move the light with the camera
         scene.registerBeforeRender(function () {
             light.position = camera.position;
         });
 
-        assetsManager.onFinish = function (tasks) {
+        assetsManager.onFinish = (tasks) => {
             engine.runRenderLoop(function () {
                 scene.render();
             });
         };
 
         assetsManager.load();
-
         return scene;
     };
 
-    // call the createScene function
-    var scene = createScene();
-
-    // run the render loop
-    engine.runRenderLoop(function(){
+    let scene = createScene();
+    engine.runRenderLoop(function () {
         scene.render();
     });
 
-    // the canvas/window resize event handler
-    window.addEventListener('resize', function(){
+    window.addEventListener('resize', function () {
         engine.resize();
     });
 });
